b3bcf5844869dd391085957f4c44b1dffb9a1e5a
JackRarer <113652777+JackRarer@users.noreply.github.com>
2022-10-25 17:33:43 -0400

    initialize

---
+++ config.js
@@ -0,0 +1,7 @@
+// Put your own Twitter App keys here. See README.md for more detail.
+module.exports = {
+  consumer_key:         'fxRnTfnmDyDtkfmDRrezxQSOp',
+  consumer_secret:      'bdFi04onu8FHK4iDSvcvNnaODre2B6oNdxjUC19qhbGXPItKsA',
+  access_token:         '1584248698392891392-MGe8pHRwEIESgGSx3SO4ZSwVmsOd5y',
+  access_token_secret:  'bbgz5V4H1xwGBbQfPO0W2qTF2cJAjU0YT7sNjuSlR2W32'
+}
